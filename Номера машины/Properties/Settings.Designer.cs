a50a50d7cba1a0539b2d79e2a0ad577716ea846c
Dagir <dagir.ram@mail.ru>
2023-03-23 09:08:44 +0300

    Removed errors

--- Номера машины/Properties/Settings.Designer.cs
+++ Номера машины/Properties/Settings.Designer.cs
@@ -26,8 +26,7 @@ namespace Номера_машины.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"|DataDirectory|\\Resources\\Car Info" +
-            ".mdb\"")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"Car Info.mdb\"")]
         public string Connection {
             get {
                 return ((string)(this["Connection"]));
